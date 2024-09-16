da328c2d5deb382cc80b321633f57160522dc8be
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-09-16 14:27:05 +0200

    Set up sentry

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,30 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import * as Sentry from "@sentry/solid";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root');
+Sentry.init({
+  dsn: "https://812ebc29f11ad9f9e6e26775de973ee9@o4507492088676352.ingest.de.sentry.io/4507962492911696",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
